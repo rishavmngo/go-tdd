6179ad3dafbf3417d6c4744b51a1a678cf09d416
rishavmngo <rishavinmngo@gmail.com>
2023-09-03 17:20:21 +0530

    pointers and errors

--- pointers_errors/pointers.go
+++ pointers_errors/pointers.go
@@ -1,14 +1,40 @@
 package pointers
 
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
 type Wallet struct {
-	currentBalance int
+	balance Bitcoin
 }
 
-func (wallet Wallet) Deposit(amt int) {
-	wallet.currentBalance += amt
+// type Stringer interface {
+// 	String() string
+// }
+
+func (wallet *Wallet) Deposit(amt Bitcoin) {
+	wallet.balance += amt
 
 }
 
-func (wallet Wallet) Balance() int {
-	return wallet.currentBalance
+func (wallet *Wallet) Balance() Bitcoin {
+	return wallet.balance
+}
+
+func (b Bitcoin) String() string {
+
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (wallet *Wallet) Withdraw(amt Bitcoin) error {
+	if amt <= wallet.balance {
+		wallet.balance -= amt
+		return nil
+	}
+	return ErrInsufficientFunds
 }
--- pointers_errors/pointers_test.go
+++ pointers_errors/pointers_test.go
@@ -1,18 +1,63 @@
 package pointers
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
+
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	want := 10
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+func assertError(t testing.TB, err error, want error) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("wanted an error but didn't get one")
 	}
 
+	if err != want {
+		t.Errorf("wanted %q got %q", err, want)
+	}
+}
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
 }
