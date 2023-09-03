be5226c57a75b9aeff7d3102f61c5fc07b70af7a
rishavmngo <rishavinmngo@gmail.com>
2023-09-03 17:45:58 +0530

    dictionary

---
+++ maps/dictionary.go
@@ -0,0 +1,60 @@
+package dictionary
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update  because word did't exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	defination, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return defination, nil
+
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
+	return nil
+
+}
+
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
+}
